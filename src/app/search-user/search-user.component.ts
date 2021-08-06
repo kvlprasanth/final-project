7b09b8d25841d5b07cba7a473329a352c711f8ad
001ZQ9744 <kvlprasanth1108@gmail.com>
2021-08-06 18:12:42 +0530

    final commit

--- src/app/search-user/search-user.component.ts
+++ src/app/search-user/search-user.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../user';
+import {ActivatedRoute, Router} from '@angular/router';
+import {UserService} from '../user.service';
+import {error} from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-search-user',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchUserComponent implements OnInit {
 
-  constructor() { }
+  user: User;
 
-  ngOnInit() {
+  constructor(private router: Router, private route: ActivatedRoute, private userService: UserService) {
   }
 
+  ngOnInit(): void {
+    this.user = new User();
+  }
+
+
+  onSubmit(id: any) {
+    console.log(id);
+    this.userService.getUserById(id).subscribe(data => {
+        console.log(data);
+        this.user = data;
+        console.log(this.user);
+        this.router.navigate(['search-user', id]);
+      }
+
+      // tslint:disable-next-line:no-shadowed-variable
+      , error => console.log(error));
+  }
 }
